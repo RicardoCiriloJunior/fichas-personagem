260dae46a22836e9398d11e6491de2bb485ccfed
RicardoCiriloPicPay <ricardo.cirilo@picpay.com>
2026-01-21 10:35:24 -0300

    altera o basename

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename='/fichas-personagens'>
+    <BrowserRouter basename='/'>
       <App />
     </BrowserRouter>
   </StrictMode>,
